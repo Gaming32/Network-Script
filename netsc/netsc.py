1eb0a3178e5c6f9fd27e5d428a2b432922140cd5
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2020-01-27 17:24:15 -0600

    abc created

--- netsc/netsc.py
+++ netsc/netsc.py
@@ -1,4 +1,51 @@
+from .struct import objects2data, data2objects, return2data
+
 class NetworkScript:
-    pass
+    """Abstract class
+Must define:
+    sock : socket.socket
+    wrapped : object"""
+    attrs = []
+    chunk_size = 8192
+
+    def __init__(self):
+        raise NotImplementedError('Abstract class')
+    def __get_call(self, funcname, args=(), kwargs={}):
+        if hasattr(self, funcname):
+            getattr(self, funcname)(*args, **kwargs)
+    def _wrapped_call(self, attr, args):
+        ret = getattr(self.wrapped, attr)(*args)
+        data = return2data(ret)
+        self.sock.send(data)
+        return ret
+    def _message(self, name, args=(), kwargs={}):
+        if name in attrs:
+            self.__get_call('_pre_func_getattr')
+            self.__get_call('_attr_get', (name,))
+            self.sock.send(objects2data('getattr', name))
+            while True:
+                res = data2objects(self.sock.recv(self.chunk_size))
+                if res['return']: break
+                else:
+                    self._wrapped_call(res['name'], res['args'])
+            self.__get_call('_post_func_getattr')
+            return res['args']
+        else:
+            self.__get_call('_pre_func_%s' % name)
+            args = self.__get_call('_adj_args_%s' % name, args, kwargs)
+            self.sock.send(objects2data(name, *args))
+            while True:
+                res = data2objects(self.sock.recv(self.chunk_size))
+                if res['return']: break
+                else:
+                    self._wrapped_call(res['name'], res['args'])
+            self.__get_call('_post_func_%s' % name)
+            return res['args']
+    def poll(self):
+        while True:
+            res = data2objects(self.sock.recv(self.chunk_size))
+            if res['return']: continue
+            else: break
+        return self._wrapped_call(res['name'], res['args'])
 
 __all__ = ['NetworkScript']
